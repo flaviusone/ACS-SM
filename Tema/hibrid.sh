690854061430a262f2a2cbc267a1776a68c33f9c
Flavius Tirnacop <flavius.tirnacop@cti.pub.ro>
2015-01-05 01:01:18 +0200

    [Proiect] adaugat mpi threads

--- Tema/hibrid.sh
+++ Tema/hibrid.sh
@@ -3,9 +3,9 @@ HIBRID_OUTPUT="build/hibrid"
 module load libraries/openmpi-1.6-gcc-4.6.3
 
 echo -e "# Running test 5 - Hybrid on large file multiple threads" >> hibrid
-mpirun -n 10 ./$HIBRID_OUTPUT inputs/4.jpg out/4_HSV_MPI.jpg >> hibrid
-mpirun -n 10 ./$HIBRID_OUTPUT inputs/4.jpg out/4_HSV_MPI.jpg >> hibrid
-mpirun -n 10 ./$HIBRID_OUTPUT inputs/4.jpg out/4_HSV_MPI.jpg >> hibrid
-mpirun -n 10 ./$HIBRID_OUTPUT inputs/4.jpg out/4_HSV_MPI.jpg >> hibrid
-mpirun -n 10 ./$HIBRID_OUTPUT inputs/4.jpg out/4_HSV_MPI.jpg >> hibrid
-mpirun -n 10 ./$HIBRID_OUTPUT inputs/4.jpg out/4_HSV_MPI.jpg >> hibrid
+mpirun -n 1 ./$HIBRID_OUTPUT inputs/4.jpg out/4_HSV_MPI.jpg >> hibrid
+mpirun -n 2 ./$HIBRID_OUTPUT inputs/4.jpg out/4_HSV_MPI.jpg >> hibrid
+mpirun -n 4 ./$HIBRID_OUTPUT inputs/4.jpg out/4_HSV_MPI.jpg >> hibrid
+mpirun -n 6 ./$HIBRID_OUTPUT inputs/4.jpg out/4_HSV_MPI.jpg >> hibrid
+mpirun -n 8 ./$HIBRID_OUTPUT inputs/4.jpg out/4_HSV_MPI.jpg >> hibrid
+mpirun -n 16 ./$HIBRID_OUTPUT inputs/4.jpg out/4_HSV_MPI.jpg >> hibrid
--- Tema/mpi.sh
+++ Tema/mpi.sh
@@ -3,9 +3,9 @@ MPI_OUTPUT="build/mpi"
 module load libraries/openmpi-1.6-gcc-4.6.3
 
 echo -e "# Running test 3 - Mpi on large file multiple threads" >> mpi
-mpirun -n 10 ./$MPI_OUTPUT inputs/4.jpg out/4_HSV_MPI.jpg >> mpi
-mpirun -n 10 ./$MPI_OUTPUT inputs/4.jpg out/4_HSV_MPI.jpg >> mpi
-mpirun -n 10 ./$MPI_OUTPUT inputs/4.jpg out/4_HSV_MPI.jpg >> mpi
-mpirun -n 10 ./$MPI_OUTPUT inputs/4.jpg out/4_HSV_MPI.jpg >> mpi
-mpirun -n 10 ./$MPI_OUTPUT inputs/4.jpg out/4_HSV_MPI.jpg >> mpi
-mpirun -n 10 ./$MPI_OUTPUT inputs/4.jpg out/4_HSV_MPI.jpg >> mpi
+mpirun -n 1 ./$MPI_OUTPUT inputs/4.jpg out/4_HSV_MPI.jpg >> mpi
+mpirun -n 2 ./$MPI_OUTPUT inputs/4.jpg out/4_HSV_MPI.jpg >> mpi
+mpirun -n 4 ./$MPI_OUTPUT inputs/4.jpg out/4_HSV_MPI.jpg >> mpi
+mpirun -n 6 ./$MPI_OUTPUT inputs/4.jpg out/4_HSV_MPI.jpg >> mpi
+mpirun -n 8 ./$MPI_OUTPUT inputs/4.jpg out/4_HSV_MPI.jpg >> mpi
+mpirun -n 16 ./$MPI_OUTPUT inputs/4.jpg out/4_HSV_MPI.jpg >> mpi
