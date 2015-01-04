0797ad7232d1fe13202d9a738d6b91897576dd82
Flavius Tirnacop <flavius.tirnacop@cti.pub.ro>
2015-01-05 00:46:53 +0200

    [Proiect] same

--- Tema/hibrid.sh
+++ Tema/hibrid.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 HIBRID_OUTPUT="build/hibrid"
+module load libraries/openmpi-1.6-gcc-4.6.3
 
 echo -e "# Running test 5 - Hybrid on large file multiple threads" >> hibrid
 mpirun -n 10 ./$HIBRID_OUTPUT inputs/4.jpg out/4_HSV_MPI.jpg >> hibrid
