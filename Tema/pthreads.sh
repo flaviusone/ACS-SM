6af19129774a588405e15af70a62d10a707e1ada
Flavius Tirnacop <flavius.tirnacop@cti.pub.ro>
2015-01-05 00:36:23 +0200

    [Proiect] Scris rezutlate in fisiere separate

--- Tema/pthreads.sh
+++ Tema/pthreads.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 PTHREADS_OUTPUT="build/pthreads"
 
-echo -e "# Running test 4 - Pthreads on large file multiple threads"
-./$PTHREADS_OUTPUT 1 inputs/4.jpg out/4_HSV_OMP.jpg
-./$PTHREADS_OUTPUT 2 inputs/4.jpg out/4_HSV_OMP.jpg
-./$PTHREADS_OUTPUT 4 inputs/4.jpg out/4_HSV_OMP.jpg
-./$PTHREADS_OUTPUT 6 inputs/4.jpg out/4_HSV_OMP.jpg
-./$PTHREADS_OUTPUT 8 inputs/4.jpg out/4_HSV_OMP.jpg
-./$PTHREADS_OUTPUT 16 inputs/4.jpg out/4_HSV_OMP.jpg
+echo -e "# Running test 4 - Pthreads on large file multiple threads" >> pthreads
+./$PTHREADS_OUTPUT 1 inputs/4.jpg out/4_HSV_OMP.jpg >> pthreads
+./$PTHREADS_OUTPUT 2 inputs/4.jpg out/4_HSV_OMP.jpg >> pthreads
+./$PTHREADS_OUTPUT 4 inputs/4.jpg out/4_HSV_OMP.jpg >> pthreads
+./$PTHREADS_OUTPUT 6 inputs/4.jpg out/4_HSV_OMP.jpg >> pthreads
+./$PTHREADS_OUTPUT 8 inputs/4.jpg out/4_HSV_OMP.jpg >> pthreads
+./$PTHREADS_OUTPUT 16 inputs/4.jpg out/4_HSV_OMP.jpg >> pthreads
